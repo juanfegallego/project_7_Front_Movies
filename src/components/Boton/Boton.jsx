78a8396c3a3f8d0884bd1d23133ef1f3ec3c3b55
juanfegallego <juanfegallegodj@gmail.com>
2021-06-24 17:33:32 +0200

    button component created

---
+++ src/components/Boton/Boton.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+
+
+const Boton = (props) => {
+
+    let history = useHistory();
+
+    const llevame = () => {
+        history.push(props.lugar);
+    }
+
+    return (
+        <div className="boton" 
+        onClick={()=>llevame()}>
+            {props.destino}
+        </div>
+    )
+}
+
+export default Boton;
+
