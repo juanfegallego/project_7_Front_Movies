c4079f38f7d90e665f5bfbd4725b8c3268eecbe5
juanfegallego <juanfegallegodj@gmail.com>
2021-06-24 17:31:09 +0200

    home created

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import MoviesHome from "../../components/MoviesHome/MoviesHome"
+import MoviesComedy from '../../components/MoviesComedy/MoviesComedy';
+
+
+
+
+const Home = () => {
+
+
+    return (
+
+        <div className="vistaHome">
+            <div >
+                Home
+                <MoviesHome/>
+                <MoviesComedy/>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
