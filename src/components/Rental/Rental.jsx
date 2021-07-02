ce82373584ecee37454f2c7926adcc4d3f968fb7
juanfegallego <juanfegallegodj@gmail.com>
2021-07-02 15:35:39 +0200

    created the function to display the rentals of the users

---
+++ src/components/Rental/Rental.jsx
@@ -0,0 +1,89 @@
+import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+import axios from 'axios';
+import { RENTALS } from '../../redux/type.js';
+import moment from 'moment';
+import Spinner from '../Spinner/Spinner';
+
+const Rental = (props) => {
+
+    let history = useHistory();
+
+    const [orders, setOrders] = useState([]);
+    const [datos,setDatos] = useState({
+        token: props.credentials?.token,
+        user: props.credentials?.user,
+    });
+
+    useEffect(() => {
+        findOrders();
+    }, []);
+
+    const findOrders = () => {
+
+        let token = props.credentials?.token;
+        let body = {
+            id : datos.user.id,
+        }
+
+        axios
+        .post("http://localhost:3001/orders/orderById", body, {headers:{'authorization':'Bearer ' + token}})
+        .then((res) => {
+
+            setOrders(res.data);
+
+            props.dispatch({ type: RENTALS, payload: res?.data });
+
+        })
+        .catch((err) => {
+            console.log('Err');
+        });
+    }
+
+    const baseImgUrl = "https://image.tmdb.org/t/p";
+    const size = "w300";
+
+    const clickHandler = (MovieInfo) => {
+        //GUARDANDO EN REDUX
+       props.dispatch({ type: RENTALS, payload: MovieInfo});
+       history.push("/movieInfo");
+   };
+
+    if (orders[0]?.id) {
+
+        return (
+            <div className="allOrders">
+
+            <div className="orderContent">
+
+                {orders.map((order, index) => (
+
+                    <div key={index} className="orderCards">
+
+                        <img src={`${baseImgUrl}/${size}${order.poster_path}`} alt="posterCuliao" onClick={()=>clickHandler(order)}/>
+                        <div className="info">
+                            <p className="order"> Rental Date : { moment (order.rentalDate).format('LL')} </p>
+                            <p className="order"> Return Date : { moment (order.returnDate).format('LL')} </p>
+                        </div>
+
+                    </div>
+                ))}
+        </div>
+        </div>
+    );
+        } else {
+            return (
+              <div className="spinnerContainer">
+                <div className="spinner">
+                <Spinner/>
+                </div>
+              </div>
+            );
+        }
+}
+
+export default connect((state)=>({
+    credentials: state.credentials,
+    infoUser: state.infoUser
+}))(Rental);
