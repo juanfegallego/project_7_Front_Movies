bb6f81234892519d25b04a77e234b3b60e49a6a0
juanfegallego <juanfegallegodj@gmail.com>
2021-07-05 00:18:26 +0200

    change in the search Info code to improve the function

--- src/containers/SearchInfo/SearchInfo.jsx
+++ src/containers/SearchInfo/SearchInfo.jsx
@@ -1,21 +1,73 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { ADD_MOVIE } from "../../redux/type";
+import Spinner from "../../components/Spinner/Spinner";
 
 
 const SearchInfo = (props) => {
 
     let history = useHistory();
+  const [searchView, setsearchView] = useState([]);
 
-    const llevame = () => {
-        history.push(props.lugar);
+  const baseImgUrl = "https://image.tmdb.org/t/p"
+  const size = "w200" 
+
+  useEffect(() => {
+    setsearchView(props.movies);
+  }, []);
+
+  const selectMovie = async (movie) => {
+    try {
+      props.dispatch({ type: ADD_MOVIE, payload: movie });
+      setTimeout(() => {
+        history.push("/movieInfo");
+      }, 500);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
+  if (props.movie[0]?.id){
     return (
-        <div className="boton" onClick={()=>llevame()}>
-            {props.destino}
+      <div className="allOrders">
+        <div className="orderContent">
+          {props.movie.map((searchView, index) => {
+            return (
+              <div className="orderCards" key={index}>
+                <img
+                  className="poster_path"
+                  // src={!searchView.poster_path `https://image.tmdb.org/t/p/w200/${searchView.poster_path}`}
+                  src={`${baseImgUrl}/${size}${searchView.poster_path}`} alt="poster"
+                  onClick={() => selectMovie(searchView)} alt="poster_path"
+                />
+  
+                <div className="voteAverage">
+                  <FontAwesomeIcon className="faStart" icon={faStar} />
+                  {searchView.vote_average}/10
+                </div>
+              </div>
+            );
+          })}
+    
         </div>
+      </div>
+    ); 
+  }else {
+    return(
+      <Spinner/>
     )
-}
+  }
+  
+};
 
-export default SearchInfo;
+export default connect((state) => ({
+  credentials: state.credentials,
+  movie: state.movie,
+}))(SearchInfo);
+
 
